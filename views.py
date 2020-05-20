1d375e0b3b5a01480ee38436500b9406e693c0a8
sergeytrubin <strubin@gmail.com>
2020-05-20 14:39:11 +0300

    Added some minor changes to routes - moved the Model to models.py

--- views.py
+++ views.py
@@ -4,25 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from functools import wraps
 from forms import AddTaskForm
-import logging
-from logging import Formatter, FileHandler
+
+# condif
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-
-class Task(db.Model):
-    __tablename__ = 'tasks'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    due_date = db.Column(db.String(), nullable=False)
-    priority = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.Integer, nullable=False)
-
-    # def __repr__(self):
-    #     #return f'<Task: {self.id} {self.name}>'
-
+from models import Task
 
 migrate = Migrate(app, db)
 
@@ -38,7 +27,7 @@ def login_required(test):
     return wrap
 
 
-@app.route('/logout')
+@app.route('/logout/')
 def logout():
     session.pop('logged_in', None)
     flash('Goodbye!')
@@ -47,6 +36,7 @@ def logout():
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] or \
                 request.form['password'] != app.config['PASSWORD']:
@@ -61,24 +51,10 @@ def login():
 @app.route('/tasks/')
 @login_required
 def tasks():
-    open_tasks = [
-        dict(
-            name=obj.name,
-            due_date=obj.due_date,
-            priority=obj.priority,
-            task_id=obj.id
-            ) for obj in Task.query.filter(Task.status == 1).all()
-    ]
-
-    closed_tasks = [
-        dict(
-            name=obj.name,
-            due_date=obj.due_date,
-            priority=obj.priority,
-            task_id=obj.id
-            ) for obj in Task.query.filter(Task.status == 0).all()
-    ]
-
+    open_tasks = db.session.query(Task).filter(Task.status == 1) \
+        .order_by(Task.due_date.asc())
+    closed_tasks = db.session.query(Task).filter(Task.status == 0) \
+        .order_by(Task.due_date.asc())
     return render_template(
         'tasks.html',
         form=AddTaskForm(request.form),
@@ -90,36 +66,29 @@ def tasks():
 @app.route('/add/', methods=['POST'])
 @login_required
 def new_task():
-    name = request.form['name']
-    print(name)
-    date = request.form['due_date']
-    print(date)
-    priority = request.form['priority']
-    print(priority)
-    if not name or not date or not priority:
-        flash("All fields are required. Please try again.")
+    error = False
+    form = AddTaskForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit:
+            try:
+                new_task = Task(
+                    name=request.form['name'],
+                    due_date=request.form['due_date'],
+                    priority=request.form['priority'],
+                    status=1
+                )
+                db.session.add(new_task)
+                db.session.commit()
+            except:
+                error = True
+                db.session.rollback
+            finally:
+                db.session.close()
+                flash("New entry was succesfully posted!")
+    if not error:
         return redirect(url_for('tasks'))
     else:
-        error = False
-        try:
-            new_task = Task(
-                name=request.form['name'],
-                due_date=request.form['due_date'],
-                priority=request.form['priority'],
-                status=1
-            )
-            db.session.add(new_task)
-            db.session.commit()
-        except:
-            error = True
-            db.session.rollback
-        finally:
-            db.session.close()
-            flash("New entry was succesfully posted!")
-        if not error:
-            return redirect(url_for('tasks'))
-        else:
-            abort (500)
+        abort(500)
 
 
 # Mark tasks as complete
@@ -127,8 +96,7 @@ def new_task():
 @login_required
 def complete(task_id):
     try:
-        task = Task.query.filter_by(id=task_id).first()
-        task.status = 0
+        db.session.query(Task).filter_by(task_id=task_id).update({"status": "0"})
         db.session.commit()
     except:
         db.session.rollback()
@@ -143,7 +111,7 @@ def complete(task_id):
 @login_required
 def delete_entry(task_id):
     try:
-        Task.query.filter_by(id=task_id).delete()
+        Task.query.filter_by(task_id=task_id).delete()
         db.session.commit()
     except:
         db.session.rollback()
@@ -151,18 +119,6 @@ def delete_entry(task_id):
         db.session.close()
         flash('The task was deleted.')
     return redirect(url_for('tasks'))
-
-
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
 
 
 if __name__ == '__main__':
